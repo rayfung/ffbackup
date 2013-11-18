3f2be0c73bdb8680417d582a5d2d3253abae4b19
Ray Fung <posixfung@163.com>
2013-11-18 16:21:02 +0800

    add installation script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+INST_PATH=/opt/ffbackup
+CONFIG_PATH=/etc/ffbackup
+
+set -e
+# set -x
+
+if [ $UID -ne 0 ]; then
+    echo 'fatal error: must be run as root!'
+    exit 1
+fi
+
+cd `dirname "$0"`
+echo 'copying files...'
+mkdir -p "$INST_PATH"
+mkdir -p "$CONFIG_PATH"
+cp -v -f -t "$INST_PATH" ffbackup-server
+cp -v -f -t "$CONFIG_PATH" server.conf
+
+echo
+echo 'installation finished'
