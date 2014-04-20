87b9b53183c966b5a8080f5c0404f1a36f5f87ff
Ray Fung <posixfung@163.com>
2014-04-20 15:28:57 +0800

    installer: set up init script

--- install.sh
+++ install.sh
@@ -17,10 +17,14 @@ mkdir -p "$INST_PATH"
 mkdir -p "$CONFIG_PATH"
 cp -v -f -t "$INST_PATH" ffbackup-server
 cp -v -n -t "$CONFIG_PATH" server.conf
+cp -v -f -t /etc/init.d ffbackup-server.sh
 
 echo
 echo 'creating user ffbackup'
 id -u ffbackup >/dev/null 2>&1 || useradd -c 'FFBackup System' -d /nonexistent -M -r -s /bin/false ffbackup
+
+echo 'setting up init script'
+update-rc.d ffbackup-server.sh defaults 99
 
 echo
 echo 'installation finished'
